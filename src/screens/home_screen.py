da79f3e513a8146c1e27713e6914e8125aa9ce1e
Ashish Ligade <ashishligade9843@gmail.com>
2026-09-08 10:16:14 +0530

    basic layout

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 def home_screen():
     st.header("Home Screen")
+
+    col1,col2 = st.columns(2)
+
+    with col1:
+        if st.button('Teacher Portal',width='stretch'):
+            st.session_state['login_type'] = 'teacher'
+            st.rerun()
+    with col2:
+        if st.button('Student Portal',width='stretch'):
+            st.session_state['login_type'] = 'student'
+            st.rerun()
